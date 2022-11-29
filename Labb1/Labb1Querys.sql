0dd353a090984b0ac3e6d66f7c3878ddca93a85c
pinkros <arvid.leimar@iths.se>
2022-11-29 20:41:38 +0100

    Update Labb1Querys.sql

--- Labb1/Labb1Querys.sql
+++ Labb1/Labb1Querys.sql
@@ -1,5 +1,21 @@
---CREATE DATABASE Labb1Db;
+CREATE DATABASE Labb1Db;
 
---USE Labb1Db;
+USE Labb1Db;
+
+--CREATE TABLE tbl_Författare(
+--ID int IDENTITY(1,1) PRIMARY KEY,
+--Förnamn nvarchar(max),
+--Efternamn nvarchar(max),
+--Födelsedatum datetime
+--);
+
+--CREATE TABLE tbl_Böcker(
+--ISBN nvarchar(13) PRIMARY KEY,
+--Titel nvarchar(max),
+--Språk nvarchar(max),
+--Pris int,
+--Utgivningsdatum datetime,
+--FOREIGN KEY (FörfattarID) REFERENCES tbl_Författare(ID)
+--)
 
 
