9eaef5c8096e5ab7598ce7c8c6cc1cc911f24068
pinkros <arvid.leimar@iths.se>
2022-12-12 13:26:20 +0100

    savé

---
+++ DBÖvningar/EFLiveDemo/EFLiveDemo/DAL/HeroContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFLiveDemo.DAL;
+
+public class HeroContext : DbContext
+{
+    public DbSet<Hero> Heroes { get; set; }
+
+    public DbSet<Power> Powers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        //Connection string från SQL Server Explorer, skriv in Initial Catalog manuellt (namn = DB namn)
+        optionsBuilder.UseSqlServer("Data Source=LAPTOP-MRHJ7U09;Initial Catalog=HeroDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        //I Package manager console: först add-migration [förslagsvis "init"], sen update-database för att skapa DB.
+    }
+}
---
+++ DBÖvningar/EFLiveDemo/EFLiveDemo/Migrations/20221212100036_init.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFLiveDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Heroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeroName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Alignment = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heroes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Powers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Powers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HeroPower",
+                columns: table => new
+                {
+                    HeroesId = table.Column<int>(type: "int", nullable: false),
+                    PowersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeroPower", x => new { x.HeroesId, x.PowersId });
+                    table.ForeignKey(
+                        name: "FK_HeroPower_Heroes_HeroesId",
+                        column: x => x.HeroesId,
+                        principalTable: "Heroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HeroPower_Powers_PowersId",
+                        column: x => x.PowersId,
+                        principalTable: "Powers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HeroPower_PowersId",
+                table: "HeroPower",
+                column: "PowersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HeroPower");
+
+            migrationBuilder.DropTable(
+                name: "Heroes");
+
+            migrationBuilder.DropTable(
+                name: "Powers");
+        }
+    }
+}
